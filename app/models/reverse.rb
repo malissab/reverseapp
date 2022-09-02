f81b6194fba74fff7639d7b242fcb72c2f1d0faf
malissab <malissambishop7@gmail.com>
2022-09-02 09:09:05 -0700

    working solution

--- app/models/reverse.rb
+++ app/models/reverse.rb
@@ -1,12 +1,12 @@
 class Reverse < ApplicationRecord
     def reverse_word(s)
-        puts "what's your rev word: "
-        rev = ""
-        str.length.times{|i| 
-            rev+= i.even? ? str[i] : str[-i-1]}
-        rev
+        result = ''
+        odd_check = s.length.odd? ? s.length - 1 : s.length
+        s.length.times {|i|
+            result += i.even? ? s[i] : s[odd_check - i]}
+        result
     end
-
+        
 end
 
 puts reverse_word('hello world')
